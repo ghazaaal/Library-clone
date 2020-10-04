031169e21e0c8389f4ff5eb8b8fc43b719e9f1c7
ghazaaal <39712140+ghazaaal@users.noreply.github.com>
2020-10-04 15:51:31 +0330

    header component is updated to show email userName.

---
+++ Header.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import logo from './logo.png'
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import {useStateValue} from "./StateProvider";
+import {auth} from "./firebase";
+
+import './Header.css'
+function Header() {
+    const [{basket , user}]=useStateValue();
+    const login=()=>{
+        if(user){
+            auth.signOut();
+        }
+
+    };
+    return (
+        <nav className="header">
+            <div onClick={login} className={"profile__info"}>
+                <h5>{user?.email}</h5>
+                <AccountCircleIcon className={"profile__icon"}/>
+
+            </div>
+
+            <img className="header__logo" src={logo} alt={"NavBarLogo"}/>
+
+
+        </nav>
+    );
+}
+
+export default Header;
